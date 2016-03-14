8a62a1e261002af727a929da961756b271ca1348
Donald Faul <faudo01@ca.com>
2016-03-14 12:44:59 -0700

    14 March 2016 TC06 job
    TC06 job changes.

--- githubreport.sh
+++ githubreport.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-cd /c/Users/faudo01/Documents/Github/githubreports
+cd /c/Users/toolsadmin/Documents/Github/githubreports
 ssh -p 122 admin@github-isl-01.ca.com "ghe-org-admin-promote -u toolsadmin"
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+1000; fi
 now="$(date +'%Y_%m_%d_%H_%M_%S')"
-ruby attestation_by_owner.rb 368ee15880d5012836d2f4282f66d62f6c7d260c > /c/Users/faudo01/Documents/CSCR/githubreports/attestation_$now.tsv
+ruby attestation_by_owner.rb 368ee15880d5012836d2f4282f66d62f6c7d260c > /c/Users/toolsadmin/Documents/CSCR/githubreports/attestation_$now.tsv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+2000; fi
 
 #find "/c/Users/toolsadmin/Downloads" -name "all-*.csv" -exec rm {} \;
@@ -34,7 +34,7 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc+2000; fi
 #sftp -b /c/Users/faudo01/Documents/CSCR/githubreport_sftp2.txt -oPort=122 admin@github-isl-01.ca.com
 #ssh -p 122 admin@github-isl-01.ca.com "sudo rm /tmp/*.csv" 
 
-cd /c/Users/faudo01/Documents/CSCR/githubreports
+cd /c/Users/toolsadmin/Documents/CSCR/githubreports
 curl -u "toolsadmin:368ee15880d5012836d2f4282f66d62f6c7d260c" https://github-isl-01.ca.com/stafftools/reports/all_repositories.csv > all_repositories_$now.csv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+3000; fi
 curl -u "toolsadmin:368ee15880d5012836d2f4282f66d62f6c7d260c" https://github-isl-01.ca.com/stafftools/reports/all_organizations.csv > all_organizations_$now.csv
