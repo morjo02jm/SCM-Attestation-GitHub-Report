d9bc08b69d0be2ed7dda74b610d67af684ea6b4d
Donald Faul <faudo01@ca.com>
2016-03-14 13:13:48 -0700

    14 March 2016 Parameterize Github Access token
    Parameterize Github Access token

--- githubreport.sh
+++ githubreport.sh
@@ -3,7 +3,7 @@ cd /c/Users/toolsadmin/Documents/Github/githubreports
 ssh -p 122 admin@github-isl-01.ca.com "ghe-org-admin-promote -u toolsadmin"
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+1000; fi
 now="$(date +'%Y_%m_%d_%H_%M_%S')"
-ruby attestation_by_owner.rb 368ee15880d5012836d2f4282f66d62f6c7d260c > /c/Users/toolsadmin/Documents/CSCR/githubreports/attestation_$now.tsv
+ruby attestation_by_owner.rb $1 > /c/Users/toolsadmin/Documents/CSCR/githubreports/attestation_$now.tsv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+2000; fi
 
 #find "/c/Users/toolsadmin/Downloads" -name "all-*.csv" -exec rm {} \;
@@ -35,11 +35,11 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc+2000; fi
 #ssh -p 122 admin@github-isl-01.ca.com "sudo rm /tmp/*.csv" 
 
 cd /c/Users/toolsadmin/Documents/CSCR/githubreports
-curl -u "toolsadmin:368ee15880d5012836d2f4282f66d62f6c7d260c" https://github-isl-01.ca.com/stafftools/reports/all_repositories.csv > all_repositories_$now.csv
+curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_repositories.csv > all_repositories_$now.csv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+3000; fi
-curl -u "toolsadmin:368ee15880d5012836d2f4282f66d62f6c7d260c" https://github-isl-01.ca.com/stafftools/reports/all_organizations.csv > all_organizations_$now.csv
+curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_organizations.csv > all_organizations_$now.csv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+4000; fi
-curl -u "toolsadmin:368ee15880d5012836d2f4282f66d62f6c7d260c" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
+curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+5000; fi
 
 exit 0
