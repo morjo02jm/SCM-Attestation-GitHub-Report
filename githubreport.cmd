a9635b390ee6c0fb6954507333d72747112bb8a5
Donald Faul <faudo01@ca.com>
2016-03-14 13:59:54 -0700

    14 March 2016 wrap access token in githubreport.cmd
    wrap access token in githubreport.cmd

--- githubreport.cmd
+++ githubreport.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\Git\git-bash.exe" -c "/c/Users/faudo01/Documents/CSCR/githubreport.sh"
+"C:\Program Files\Git\git-bash.exe" -c "/c/Users/faudo01/Documents/CSCR/githubreport.sh %GITHUB_ACCESS_TOKEN%"
