32278486f37fcdf214641af93e2dafbca7fb222a
Donald Faul <faudo01@ca.com>
2016-03-14 14:09:28 -0700

    14 March 2016 Corrections
    Corrections

--- githubreport.cmd
+++ githubreport.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\Git\git-bash.exe" -c "/c/Users/faudo01/Documents/CSCR/githubreport.sh %GITHUB_ACCESS_TOKEN%"
+"C:\Program Files\Git\git-bash.exe" -c "/c/Users/toolsadmin/Documents/Github/githubreports/githubreport.sh %GITHUB_ACCESS_TOKEN%"
