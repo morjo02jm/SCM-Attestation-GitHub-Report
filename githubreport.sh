11e0332e6d5ab8b7d25193cdff96104f8c26a021
Donald Faul <faudo01@ca.com>
2016-04-15 16:41:48 -0700

    15 April 2016 Increase wait factor for user report
    Increase wait factor for user report

--- githubreport.sh
+++ githubreport.sh
@@ -7,33 +7,6 @@ now="$(date +'%Y_%m_%d_%H_%M_%S')"
 ruby attestation_by_owner.rb $1 > /c/Users/toolsadmin/Documents/CSCR/githubreports/attestation_$now.tsv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+2000; fi
 
-#find "/c/Users/toolsadmin/Downloads" -name "all-*.csv" -exec rm {} \;
-#"/c/Program Files (x86)/Mozilla Firefox/firefox.exe" -P "toolsadmin" -url https://github-isl-01.ca.com/stafftools/reports/all_users.js &
-#sleep 10
-#output=`ps -ef | grep firefox`
-#set -- $output
-#pid=$2
-#kill $pid
-#"/c/Program Files (x86)/Mozilla Firefox/firefox.exe" -P "toolsadmin" -url https://github-isl-01.ca.com/stafftools/reports/all_organizations.js &
-#sleep 10
-#output=`ps -ef | grep firefox`
-#set -- $output
-#pid=$2
-#kill $pid
-#"/c/Program Files (x86)/Mozilla Firefox/firefox.exe" -P "tooolsadmin" -url https://github-isl-01.ca.com/stafftools/reports/all_repositories.js &
-#sleep 10
-#output=`ps -ef | grep firefox`
-#set -- $output
-#pid=$2
-#kill $pid
-#
-#cp /c/Users/toolsadmin/Downloads/all-*.csv /c/Users/faudo01/Documents/Github/DevelopmentAccess/githubreports/
-
-#cd /c/Users/faudo01/Documents/CSCR
-#sftp -b /c/Users/faudo01/Documents/CSCR/githubreport_sftp1.txt -oPort=122 admin@github-isl-01.ca.com
-#ssh -p 122 admin@github-isl-01.ca.com "bash /tmp/ca-custom-all-users.sh" 
-#sftp -b /c/Users/faudo01/Documents/CSCR/githubreport_sftp2.txt -oPort=122 admin@github-isl-01.ca.com
-#ssh -p 122 admin@github-isl-01.ca.com "sudo rm /tmp/*.csv" 
 
 # Note: because the initial curl call can result in an indirection, the curl calls are run multiple times
 cd /c/Users/toolsadmin/Documents/CSCR/githubreports
@@ -62,25 +35,17 @@ sleep 10
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_organizations.csv > all_organizations_$now.csv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+4000; fi
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
+sleep 30
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
+sleep 30
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
+sleep 30
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
+sleep 30
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
+sleep 30
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
-curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
-curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
-curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
-curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-sleep 10
+sleep 30
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
 rc=$?; if [[ $rc != 0 ]]; then exit $rc+5000; fi
 
