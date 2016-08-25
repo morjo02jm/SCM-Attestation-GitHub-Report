3751fced69a6683dbeded7fa2b1d3763579a2cdc
Donald Faul <faudo01@ca.com>
2016-08-24 20:51:42 -0700

    24 August 2016 Changes
    TA439858
    Add team owner repositories
    Add collaborators

--- attestation_by_owner.rb
+++ attestation_by_owner.rb
@@ -21,6 +21,7 @@ ghe = Octokit::Client.new(
 )
 
 ghe.auto_paginate = true
+#STDOUT.sync=true
 
 # Write the TSV header
 
@@ -36,22 +37,61 @@ ghe.all_users().each do |org|
 	if org.type == 'Organization'
 		admins = ghe.organization_members(org.login, { :role => 'admin' }).map(&:login)
 
+## repositories by organization		
+		begin
+		  orepos = ghe.org_repositories(org.login, {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |r| [r.name, r.permissions, r.private, r.owner, r.id] }
+		rescue  Octokit::NotFound
+		  orepos = ['']
+		end
+
+		orepos.each do |repo|
+			ghe.organization_members(org.login).each do |member|
+			# show owners
+			    if (admins.include?(member.login)) 
+				#if (member.login == repo[3]["login"])
+					org_admin = 'Yes'
+					push = repo[1]["push"] ? 'Yes' : 'No'
+					pull = repo[1]["pull"] ? 'Yes' : 'No'
+					admin = repo[1]["admin"] ? 'Yes' : 'No'
+					type = repo[2]? 'Private' : 'Public'
+					puts "#{org.login}\t***Organization***\t#{repo[0]}\t#{type}\t#{member.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
+				end # admin
+			end # each org member
+			
+			# show collaborators
+			begin
+				collabs = ghe.collabs(repo[4], {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |c| [c.login, c.permissions]}
+			rescue  Octokit::NotFound
+				collabs = ['']
+			end
+			
+			collabs.each do |collab|
+				org_admin = 'No'
+				push  = collab[1]["push"] ? 'Yes' : 'No'
+				pull  = collab[1]["pull"] ? 'Yes' : 'No'
+				admin = collab[1]["admin"] ? 'Yes' : 'No'
+				type = repo[2]? 'Private' : 'Public'
+				puts "#{org.login}\t***Collaborator***\t#{repo[0]}\t#{type}\t#{collab[0]}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"				
+			end # collaborators
+		end #each repo
+				
+# repositories by teams	
 		ghe.org_teams(org.login).each do |team|
 			begin
-			  repos = ghe.team_repositories(team.id, {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |r| [r.name, r.permissions, r.private] }
+			  trepos = ghe.team_repositories(team.id, {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |r| [r.name, r.permissions, r.private] }
 			rescue  Octokit::NotFound
-			  repos = ['']
+			  trepos = ['']
 			end
 
 			ghe.team_members(team.id).each do |member|
-			  repos.each do |repo|
-				push = repo[1]["push"] ? 'Yes' : 'No'
-				pull = repo[1]["pull"] ? 'Yes' : 'No'
-				admin = repo[1]["admin"] ? 'Yes' : 'No'
-				org_admin = admins.include?(member.login) ? 'Yes' : 'No'
-				type = repo[2]? 'Private' : 'Public'
-				puts "#{org.login}\t#{team.name}\t#{repo[0]}\t#{type}\t#{member.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
-			  end #each repo
+			    trepos.each do |repo|
+					push = repo[1]["push"] ? 'Yes' : 'No'
+					pull = repo[1]["pull"] ? 'Yes' : 'No'
+					admin = repo[1]["admin"] ? 'Yes' : 'No'
+					org_admin = admins.include?(member.login) ? 'Yes' : 'No'
+					type = repo[2]? 'Private' : 'Public'
+					puts "#{org.login}\t#{team.name}\t#{repo[0]}\t#{type}\t#{member.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
+			    end #each repo
 			end #each team member
 		end #each team
 	else #org.type == 'User'
@@ -62,15 +102,15 @@ ghe.all_users().each do |org|
 		end
 		
         repos.each do |repo|
-				orgname = '***User Repository***'
-		        team = org.login
-				org_admin = 'N/A'
-                reponame = repo[0]
-				push = repo[1]["push"] ? 'Yes' : 'No'
-				pull = repo[1]["pull"] ? 'Yes' : 'No'
-				admin = repo[1]["admin"] ? 'Yes' : 'No'
-				type = repo[2]? 'Private' : 'Public'
-				puts "#{orgname}\t#{team}\t#{reponame}\t#{type}\t#{org.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"		
+			orgname = '***User Repository***'
+			team = org.login
+			org_admin = 'N/A'
+			reponame = repo[0]
+			push = repo[1]["push"] ? 'Yes' : 'No'
+			pull = repo[1]["pull"] ? 'Yes' : 'No'
+			admin = repo[1]["admin"] ? 'Yes' : 'No'
+			type = repo[2]? 'Private' : 'Public'
+			puts "#{orgname}\t#{team}\t#{reponame}\t#{type}\t#{org.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"		
         end # user repos	
 	end #check user type
 end #each Organization
