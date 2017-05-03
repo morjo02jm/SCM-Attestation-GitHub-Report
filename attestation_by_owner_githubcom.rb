40ab43cc589d0ae408215bff3b8f7ec4bd170928
Donald Faul <faudo01@ca.com>
2017-05-03 16:21:41 -0700

    faudo01 US331302
    US331302 Modify githubcom ruby script to cover all organizations where
    ca-toolsadmin belongs

--- attestation_by_owner_githubcom.rb
+++ attestation_by_owner_githubcom.rb
@@ -25,7 +25,7 @@ ghe.auto_paginate = true
 
 # Write the TSV header
 
-puts "Organization\tTeam\tRepository\tRepository Type\tUser ID\tCan Push\tCan Pull\tCan Admin\tIs Organization Admin"
+#puts "Organization\tTeam\tRepository\tRepository Type\tUser ID\tCan Push\tCan Pull\tCan Admin\tIs Organization Admin"
 
 
 # We use the Octokit interface to call the GitHub API to 
@@ -34,81 +34,100 @@ puts "Organization\tTeam\tRepository\tRepository Type\tUser ID\tCan Push\tCan Pu
 #  c) for user repositories, we fetch the permissions
 
 
-org = ghe.org('RallySoftware')
-if org.type == 'Organization'
-	admins = ghe.organization_members(org.login, { :role => 'admin' }).map(&:login)
-				
+#org = ghe.org('RallySoftware')
+#ghe.list_orgs().each do |org1|
+def print_orgs(ghe, orgs)
+  orgs.each do |org1|
+
+	org = ghe.org(org1.login)
+    if org.type == 'Organization'
+		admins = ghe.organization_members(org.login, { :role => 'admin' }).map(&:login)
+					
 # repositories by teams	
-	ghe.org_teams(org.login).each do |team|
-		begin
-		  trepos = ghe.team_repositories(team.id, {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |r| [r.name, r.permissions, r.private] }
-		rescue  Octokit::NotFound
-		  trepos = ['']
-		end
-		
-        last = ""
-		ghe.team_members(team.id).each do |member|
-			trepos.each do |repo|
-				push = repo[1]["push"] ? 'Yes' : 'No'
-				pull = repo[1]["pull"] ? 'Yes' : 'No'
-				admin = repo[1]["admin"] ? 'Yes' : 'No'
-				org_admin = admins.include?(member.login) ? 'Yes' : 'No'
-				type = repo[2]? 'Private' : 'Public'
-				this = "#{org.login}\t#{team.name}\t#{repo[0]}\t#{type}\t#{member.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
-				if (!(this == last))
-					puts this
-				end
-				last = this
-			end #each repo
-		end #each team member
-	end #each team
+		ghe.org_teams(org.login).each do |team|
+			begin
+			  trepos = ghe.team_repositories(team.id, {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |r| [r.name, r.permissions, r.private] }
+			rescue  Octokit::NotFound
+			  trepos = ['']
+			end
+			
+			last = ""
+			ghe.team_members(team.id).each do |member|
+				trepos.each do |repo|
+					push = repo[1]["push"] ? 'Yes' : 'No'
+					pull = repo[1]["pull"] ? 'Yes' : 'No'
+					admin = repo[1]["admin"] ? 'Yes' : 'No'
+					org_admin = admins.include?(member.login) ? 'Yes' : 'No'
+					type = repo[2]? 'Private' : 'Public'
+					this = "#{org.login}\t#{team.name}\t#{repo[0]}\t#{type}\t#{member.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
+					if (!(this == last))
+						puts this
+					end
+					last = this
+				end #each repo
+			end #each team member
+		end #each team
 
 # repositories by organization		
-	begin
-	  orepos = ghe.org_repositories(org.login, {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |r| [r.name, r.permissions, r.private, r.owner, r.id] }
-	rescue  Octokit::NotFound
-	  orepos = ['']
-	end
-
-	orepos.each do |repo|
-		ghe.organization_members(org.login).each do |member|
-		# show owners
-			if (admins.include?(member.login)) 
-			#if (member.login == repo[3]["login"])
-				org_admin = 'Yes'
-				push = repo[1]["push"] ? 'Yes' : 'No'
-				pull = repo[1]["pull"] ? 'Yes' : 'No'
-				admin = repo[1]["admin"] ? 'Yes' : 'No'
-				type = repo[2]? 'Private' : 'Public'
-				puts "#{org.login}\t***Owner***\t#{repo[0]}\t#{type}\t#{member.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
-			end # admin
-		end # each org member
-		
-		# show collaborators
 		begin
-			collabs = ghe.collabs(repo[4], {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |c| [c.login, c.permissions]}
+		  orepos = ghe.org_repositories(org.login, {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |r| [r.name, r.permissions, r.private, r.owner, r.id] }
 		rescue  Octokit::NotFound
-			collabs = ['']
-		rescue Octokit::RepositoryUnavailable
-			collabs = ['']
+		  orepos = ['']
 		end
-		
-		collabs.each do |collab|
-			if (!admins.include?(collab[0]))
-				org_admin = 'No'
-				if (collab[1] != nil)
-					push  = collab[1]["push"] ? 'Yes' : 'No'
-					pull  = collab[1]["pull"] ? 'Yes' : 'No'
-					admin = collab[1]["admin"] ? 'Yes' : 'No'
-				else
-					push  = 'No'
-					pull  = 'No'
-					admin = 'No'
-				end 
-				type = repo[2]? 'Private' : 'Public'
-				puts "#{org.login}\t***Collaborator***\t#{repo[0]}\t#{type}\t#{collab[0]}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
-			end # not owner			
-		end # collaborators
-	end #each repo
-end #check user type
 
+		orepos.each do |repo|
+			ghe.organization_members(org.login).each do |member|
+			# show owners
+				if (admins.include?(member.login)) 
+				#if (member.login == repo[3]["login"])
+					org_admin = 'Yes'
+					push = repo[1]["push"] ? 'Yes' : 'No'
+					pull = repo[1]["pull"] ? 'Yes' : 'No'
+					admin = repo[1]["admin"] ? 'Yes' : 'No'
+					type = repo[2]? 'Private' : 'Public'
+					puts "#{org.login}\t***Owner***\t#{repo[0]}\t#{type}\t#{member.login}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
+				end # admin
+			end # each org member
+			
+# show collaborators
+			begin
+				collabs = ghe.collabs(repo[4], {:accept => 'application/vnd.github.ironman-preview+json'}).map{ |c| [c.login, c.permissions]}
+			rescue  Octokit::NotFound
+				collabs = ['']
+			rescue Octokit::RepositoryUnavailable
+				collabs = ['']
+			end
+			
+			collabs.each do |collab|
+				if (!admins.include?(collab[0]))
+					org_admin = 'No'
+					if (collab[1] != nil)
+						push  = collab[1]["push"] ? 'Yes' : 'No'
+						pull  = collab[1]["pull"] ? 'Yes' : 'No'
+						admin = collab[1]["admin"] ? 'Yes' : 'No'
+					else
+						push  = 'No'
+						pull  = 'No'
+						admin = 'No'
+					end 
+					type = repo[2]? 'Private' : 'Public'
+					puts "#{org.login}\t***Collaborator***\t#{repo[0]}\t#{type}\t#{collab[0]}\t#{push}\t#{pull}\t#{admin}\t#{org_admin}"
+				end # not owner			
+			end # collaborators
+		end #each repo
+	end #check user type
+  end	
+end #loop over orgs
+
+ghe.list_orgs
+last_response = ghe.last_response
+
+puts "Organization\tTeam\tRepository\tRepository Type\tUser ID\tCan Push\tCan Pull\tCan Admin\tIs Organization Admin"
+print_orgs(ghe, last_response.data)
+
+#this piece of coding handles the pagination restriction in the ghe.orgs request
+
+until last_response.rels[:next].nil?
+  last_response = last_response.rels[:next].get
+  print_orgs(ghe, last_response.data)
+end
