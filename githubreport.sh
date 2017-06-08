a27484eff09a39d9dbada8b1b16b44f590853503
Donald Faul <faudo01@ca.com>
2017-06-08 15:35:27 -0700

    faudo01 8 June 2017
    US347003 update githubreports.sh with identity services processing

--- githubreport.sh
+++ githubreport.sh
@@ -2,12 +2,12 @@
 
 cd /c/AutoSys/Github/githubreports
 ssh -i /c/Users/toolsadmin/.ssh/id_rsa -p 122 admin@github-isl-01.ca.com "ghe-org-admin-promote -u toolsadmin"
-rc=$?; if [ $rc != 0 ]; then exit $rc+1000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+1000)); fi
 
 now="$(date +'%Y_%m_%d_%H_%M_%S')"
 dayofweek="$(date +'%w')"
 ruby attestation_by_owner.rb $1 > /c/AutoSys/CSCR/githubreports/attestation_github_$now.tsv
-rc=$?; if [ $rc != 0 ]; then exit $rc+2000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+2000)); fi
 
 ## Note: because the initial curl call can result in an indirection, the curl calls are run multiple times
 cd /c/AutoSys/CSCR/githubreports
@@ -25,7 +25,7 @@ sleep 10
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_repositories.csv > all_repositories_$now.csv
 sleep 10
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_repositories.csv > all_repositories_$now.csv
-rc=$?; if [ $rc != 0 ]; then exit $rc+3000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+3000)); fi
 fi
 
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_organizations.csv > all_organizations_$now.csv
@@ -39,7 +39,7 @@ sleep 10
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_organizations.csv > all_organizations_$now.csv
 sleep 10
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_organizations.csv > all_organizations_$now.csv
-rc=$?; if [ $rc != 0 ]; then exit $rc+4000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+4000)); fi
 
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
 sleep 60
@@ -54,7 +54,7 @@ sleep 60
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
 sleep 60
 curl -u "toolsadmin:$1" https://github-isl-01.ca.com/stafftools/reports/all_users.csv > all_users_$now.csv
-rc=$?; if [ $rc != 0 ]; then exit $rc+5000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+5000)); fi
 
 cd /c/AutoSys/CSCR
 export JAVA_HOME=$JAVA_HOME_SCM_8
@@ -67,11 +67,11 @@ else
 outfile="-outputfile /c/AutoSys/CSCR/githubreports/governance_ghe_$now.tsv" 
 fi
 java -Xmx1024m -jar githubrepldap.jar -repofile /c/AutoSys/CSCR/githubreports/attestation_github_$now.tsv -orgfile /c/AutoSys/CSCR/githubreports/all_organizations_$now.csv -userfile /c/AutoSys/CSCR/githubreports/all_users_$now.csv $outfile -log /c/AutoSys/CSCR/githubrepldap
-rc=$?; if [ $rc != 0 ]; then exit $rc+6000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+6000)); fi
 
 cd /c/AutoSys/Github/githubreports
 ruby attestation_by_owner_githubcom.rb $2 > /c/AutoSys/CSCR/githubreports/attestation_githubcom_$now.tsv
-rc=$?; if [ $rc != 0 ]; then exit $rc+7000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+7000)); fi
 
 cd /c/AutoSys/CSCR
 if [ "$dayofweek" != "0" ]
@@ -81,7 +81,7 @@ else
 outfile="-outputfile /c/AutoSys/CSCR/githubreports/governance_githubcom_$now.tsv"
 fi
 java -Xmx1024m -jar githubrepldap.jar -rally -repofile /c/AutoSys/CSCR/githubreports/attestation_githubcom_$now.tsv -userfile /c/AutoSys/CSCR/github_user_mapping.csv -remove $outfile -log /c/AutoSys/CSCR/githubrepldap
-rc=$?; if [ $rc != 0 ]; then exit $rc+8000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+8000)); fi
 
 if [ "$dayofweek" != "0" ]
 then 
@@ -90,7 +90,7 @@ else
 outfile="-outputfile /c/AutoSys/CSCR/githubreports/governance_harvest_$now.tsv"
 fi
 java -Xmx1024m -jar scmldap.jar -report $outfile -log /c/AutoSys/CSCR/scmldap
-rc=$?; if [ $rc != 0 ]; then exit $rc+9000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+9000)); fi
 
 if [ "$dayofweek" != "0" ]
 then 
@@ -99,7 +99,7 @@ else
 outfile="-outputfile /c/AutoSys/CSCR/githubreports/governance_endevor_$now.tsv"
 fi
 java -Xmx1024m -jar endevorrepldap.jar $outfile -log /c/AutoSys/CSCR/endevorrepldap
-rc=$?; if [ $rc != 0 ]; then exit $rc+10000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+10000)); fi
 
 if [ "$dayofweek" != "0" ]
 then 
@@ -108,7 +108,12 @@ else
 outfile="-outputfile /c/AutoSys/CSCR/githubreports/governance_mainframe_$now.tsv"
 fi
 java -Xmx1024m -jar zOSrepldap.jar $outfile -log /c/AutoSys/CSCR/zOSrepldap
-rc=$?; if [ $rc != 0 ]; then exit $rc+11000; fi
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+11000)); fi
+
+# github.com CA IDS
+java -Xmx1024m -jar identityservicesldap.jar -add IdentityServicesAddUsers.csv -ADgroups IdentityServicesADGroups.tsv -contacts IdentityServicesContacts.tsv -mapfile github_user_mapping.csv -updateorg -log /c/AutoSys/CSCR/identityservicesldap
+rc=$?; if [ $rc != 0 ]; then exit $(($rc+12000)); fi
+
 
 #cp -f /c/AutoSys/CSCR/githubreports/governance_github*_$now.csv /z/Reports/GovernanceMinder/
 
